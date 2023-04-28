c82353f26a58d42f136dee72b2c0ba71e34b3ea1
Dmitry <grammattika@gmail.com>
2023-04-28 16:02:13 +0300

    first commit

---
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_6._OOP._Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
