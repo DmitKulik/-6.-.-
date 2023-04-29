1beb430e7914d31bcf1658ce2e8d3f942902b18b
Dmitry <grammattika@gmail.com>
2023-04-29 15:31:38 +0300

    //Задание 6.2.8

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -9,24 +9,33 @@ namespace Module_6._OOP._Introduction
     {
         static void Main(string[] args)
         {
-            Animal animal = new Animal { type = "Собака", name = "Вольт", age = 4 };
+            //Animal animal = new Animal { type = "Собака", name = "Вольт", age = 4 };
 
         }
-        //Задание 6.2.2
-        class Pen
+        //Задание 6.2.8
+        class Rectangle
         {
-            public string color;
-            public int cost;
-        }
-        public Pen()
-        {
-            string color = "черный";
-            int cost = 100;
-        }
-        public Pen(string penColor, int penCost)
-        {
-            color = penColor;
-            cost = penCost;
+            public int a;
+            public int b;
+
+            public Rectangle()
+            {
+                int a = 6;
+                int b = 4;
+            }
+
+            public Rectangle(int side) // a == b c одним параметром
+            {
+                a = side;
+                b = side;
+            }
+
+            Rectangle(int first, int second) //с 2 параметрами, когда a != b  
+            {
+                a = first;
+                b = second;
+            }
+            public int Square(out int c) => c = a * b; // метод посчета суммы
         }
 
     }
