f58abf5bbd521a03dd2eef84202bbf62d6612a17
Dmitry <grammattika@gmail.com>
2023-04-30 15:31:06 +0300

    //Задание 6.6.1

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Drawing;
+using System.Globalization;
 using System.Xml.Linq;
 
 namespace Module_6._OOP._Introduction
@@ -9,77 +10,37 @@ namespace Module_6._OOP._Introduction
     class Program
     {
         static void Main(string[] args)
-        { 
-        }
-
-
-        enum TurnDirection //Добавили перечисление TurnDirection
-                           //для обозначения направления поворота для указателя
-                           //поворота машины (поворотник не включен, право, лево)
         {
-            None = 0,
-            Left,
-            Right
         }
+        //Задание 6.6.1
 
-        class Car
+        class TrafficLight
         {
-            private double Fuel;
+            string Color;
 
-            private int Mileage;
+        }
+        private void ChangeColor(string Color)
+        {
 
-            private string color; //добавлен цвет, используя приватное поле color
-
-            private TurnDirection turn; //Добавили закрытое поле для хранения этой информации
-
-            public Car()
-            {
-                Fuel = 50;
-                Mileage = 0;
-                color = "White";
-            }
-
-            private void Move()
-            {
-                // Move a kilometer
-                Mileage++;
-                Fuel -= 0.5;
-            }
-
-            private void Turn(TurnDirection direction) //Закрытый метод для изменения его значения, чтобы водитель мог включать и выключать поворотники 
-            {
-                turn = direction;
-            }
-
-            public void FillTheCar()
-            {
-                Fuel = 50;
-            }
-
-            public string GetColor() // метод для получения цвета
-            {
-                return color;
-            }
-
-            public void ChangeColor(string newColor) // метод для изминения цвета
-            {
-                if (color != newColor)
-                    color = newColor;
-            }
-
-            public bool IsTurningLeft() //Для внешнего же мира мы должны получать информацию о том, поворачивает ли машина влево — метод IsTurningLeft.
-            {
-                return turn == TurnDirection.Left;
-            }
-
-            public bool IsTurningRight() ////Для внешнего же мира мы должны получать информацию о том, поворачивает ли машина вправо метод IsTurningRight
-            {
-                return turn == TurnDirection.Right;
-            }
         }
 
+        public string GetColor()
+        {
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
