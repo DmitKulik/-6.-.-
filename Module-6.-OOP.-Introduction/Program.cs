7b45b0fb3b9d7bf2bcd8c751e6d1b63304d0497b
Dmitry <grammattika@gmail.com>
2023-05-02 14:34:01 +0300

    7.1.5

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -12,77 +12,55 @@ namespace Module_6._OOP._Introduction
         static void Main(string[] args)
         {
         }
-        //Задание 6.6.2
-
-        class User
+        //Задание 7.1.4
+        class Employee
         {
-            private int age;
-            private string login;
-            private string email;
-
-            public string Login
-            {
-                get
-                {
-                    return login;
-                }
-
-                set
-                {
-                    {
-                        if (login.Length < 3)
-                        {
-                            //No
-                        }
-                        else
-                        {
-                            login = value;
-                        }
-                    }
-                }
-            }
-            public string Mail
-            {
-                get
-                {
-                    return email;
-                }
-
-                set
-                {
-                    {
-                        if (!value.Contains('@')) //если значение не содержить значение
-                        {
-                            //No
-                        }
-                        else
-                        {
-                            email = value;
-                        }
-                    }
-                }
-            }
-
-            public int Age
-            {
-                get
-                {
-                    return age;
-                }
-
-                set
-                {
-                    if (value < 18)
-                    {
-                        Console.WriteLine("Возраст должен быть не меньше 18");
-                    }
-                    else
-                    {
-                        age = value;
-                    }
-                }
-            }
+            public string Name;
+            public int Age;
+            public int Salary;
         }
+
+        class ProjectManager : Employee
+        {
+            public string NameProjectName;
+            
+        }
+
+        class Developer : Employee
+        {
+            public string ProgrammingLanguage;
+
+        }
+        //Задание 7.1.5
+        class Apple
+        {
+            
+        }
+        class Banana : Apple
+        {
+            
+        }
+        class Pear : Banana
+        {
+
+        }
+        class Potato : Pear
+        {
+
+        }
+        class Carrot : Potato
+        {
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
 
