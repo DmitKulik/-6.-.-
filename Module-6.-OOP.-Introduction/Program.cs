7af9bd8971ffdb1732dd77c24b3aacf94701fa48
Dmitry <grammattika@gmail.com>
2023-05-08 18:15:30 +0300

    uuuu

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -9,43 +9,60 @@ namespace Module_6._OOP._Introduction
     {
         static void Main(string[] args)
         {
-            DerivedClass Deri = new DerivedClass();
-            
+            A a = new A();
+            a.Display();
+            B b = new B();
+            b.Display();
+            C c = new C();
+            a.Display();
+            D d = new D();
+            b.Display();
+            E e = new E();
+            c.Display();
+
         }
 
-        //Задание 7.2.4
+        //Задание 7.2.7 uuuu
 
-        class BaseClass
+        class A
         {
-            public virtual int Counter
+            public virtual void Display()
             {
-                get;
-                set;
+                Console.WriteLine("Вызван метод класса A");
+
+            }
+
+        }
+        class B : A
+        {
+            public new void Display()
+            {
+                Console.WriteLine("Вызван метод класса B");
             }
         }
-
-        class DerivedClass : BaseClass
+        class C : A
         {
-            public int counter;
-            public override int Counter
+            public override void Display()
             {
-                get
-                {
-                    return counter;
-                }
-                set
-                {
-                    if (counter > 0)
-                    {
-
-                        counter = value;
-                    }
-                }
+                Console.WriteLine("Вызван метод класса C");
+            }
+        }
+        class D : B
+        {
+            public new void Display()
+            {
+                Console.WriteLine("Вызван метод класса D");
+            }
+        }
+        class E : C
+        {
+            public new void Display()
+            {
+                Console.WriteLine("Вызван метод класса E");
             }
         }
     }
 }
-
 
 
 
