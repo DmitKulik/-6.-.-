786755e3d97036ea2f26780c572b1161b7784c85
Dmitry <grammattika@gmail.com>
2023-05-09 17:24:02 +0300

    Задание 7.3.3

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -9,86 +9,35 @@ namespace Module_6._OOP._Introduction
     {
         static void Main(string[] args)
         {
-            var array = new Book[] {
-            new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
-            new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
-        };
-            BookCollection collection = new BookCollection(array);
-
-            Console.ReadKey();
-
-            Book book = collection[1];
-            book = collection[-1];
-            book = collection[4];
-
-            Console.ReadKey();
-
-            book = collection["Мастер и Маргарита"];
-
-            Console.ReadKey();
-
+            
+           
 
         }
-        class Book
+        // Задание 7.3.3
+        abstract class ComputerPart
         {
-            public string Name;
-            public string Author;
+            public abstract void Work();
         }
-
-        class BookCollection
+        class Processor : ComputerPart
         {
-            private Book[] collection;
-
-            public BookCollection(Book[] collection)
-            {
-                this.collection = collection;
-            }
-
-            // Индексатор по массиву
-            public Book this[int index]
-            {
-                get
-                {
-                    // Проверяем, чтобы индекс был в диапазоне для массива
-                    if (index >= 0 && index < collection.Length)
-                    {
-                        return collection[index];
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-
-                private set
-                {
-                    // Проверяем, чтобы индекс был в диапазоне для массива
-                    if (index >= 0 && index < collection.Length)
-                    {
-                        collection[index] = value;
-                    }
-                }
-            }
-
-            public Book this[string name]
-            {
-                get
-                {
-                    for (int i = 0; i < collection.Length; i++)
-                    {
-                        if (collection[i].Name == name)
-                        {
-                            return collection[i];
-                        }
-                    }
-
-                    return null;
-                }
+            public override void Work() {
+                
             }
         }
-
-
-
+        class MotherBoard : ComputerPart
+        {
+            public override void Work()
+            {
+                
+            }
+        }
+        class GraphicCard : ComputerPart
+        {
+            public override void Work()
+            {
+                
+            }
+        }
     }
 }
 
