295b2c41d57ac7c6a04d453c6c9b6d933283155d
Dmitry <grammattika@gmail.com>
2023-05-10 22:25:55 +0300

    Задание 7.6.12

--- Module-6.-OOP.-Introduction/Program.cs
+++ Module-6.-OOP.-Introduction/Program.cs
@@ -9,19 +9,19 @@ namespace Module_6._OOP._Introduction
     {
         static void Main(string[] args)
         {
-            
+
         }
 
-        // Задание 7.6.6
+        // Задание 7.6.12
 
+
+        abstract class Engine { }
 
         class ElectricEngine : Engine { }
 
         class GasEngine : Engine { }
 
-        class CarPart { }
-
-        class Engine { }
+        abstract class CarPart { }
 
         class Battery : CarPart { }
 
@@ -29,16 +29,30 @@ namespace Module_6._OOP._Introduction
 
         class Wheel : CarPart { }
 
-        class Car<T1> where T1 : Engine //ограничение от класса
+        abstract class Car<TEngine> where TEngine : Engine //ограничение от класса
         {
-            public T1 Engine;
+            public TEngine Engine;
 
-            public virtual void ChangePart<T2>(T2 newPart) where T2 : CarPart //ограничение от класса
+            public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart //ограничение от класса
+            {
+
+            }
+        }
+        class ElectricCar : Car<ElectricEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
             {
 
             }
         }
 
+        class GasCar : Car<GasEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+
+            }
+        }
 
     }
 }
